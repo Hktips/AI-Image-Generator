6aa8098dc0ec7f194a9cb92be96a4cdc2b1a4e88
Hemant Kumar <88220839+Hktips@users.noreply.github.com>
2025-09-14 12:44:57 +0530

    Update multimodel_app.py

--- multimodel_app.py
+++ multimodel_app.py
@@ -32,4 +32,49 @@ if st.button("Generate Image:"):
                         st.image(image)
         except Exception as e:
             st.error(f"Image getting error: {e}")
-            
+st.title("AI Image Caption Generator")
+uploaded_image=st.file_uploader("Upload an image for caption generation", type=["png","jpg"])
+if uploaded_image:
+    image=Image.open(uploaded_image)  
+    st.image(image,caption="Uploaded Image")
+
+    if st.button("Generate Caption"):
+        try:
+            with st.spinner("Generating Caption..."):
+                response=client.models.generate_content(
+                model="gemini-2.0-flash",
+                contents=["what is this image?",image]
+            )
+                st.subheader("Generated Caption:")
+                st.write(response.text)
+
+        except Exception as e:
+            st.error("Error generating Caption")
+
+st.title("AI YouTube Video SUmmarizer")
+youtube_url=st.text_input(" Enter youtube video url")
+if st.button("Summrized youtube video"):
+    if not youtube_url:
+        st.warning("No YouTube Url Present")
+    else:
+        try:
+            with st.spinner("Summirizing YouTube Video"):
+                response = client.models.generate_content(
+                    model='models/gemini-2.5-flash',
+                    contents=types.Content(
+                        parts=[
+                            types.Part(
+                                file_data=types.FileData(file_uri=youtube_url),
+                                video_metadata=types.VideoMetadata(
+                                    start_offset='1250s',
+                                    end_offset='1570s'
+                                )
+                            ),
+                            types.Part(text='Please summarize the video in 3 sentences.')
+                        ]
+                    )
+                )
+                st.subheader("Summaring video")
+                st.write(response.text)
+        except Exception as e:
+              st.error("Error generating summary")
